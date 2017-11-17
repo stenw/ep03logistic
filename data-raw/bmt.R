6befc09aa5f2e941b545778a7dddef60173c4ea4
Sten Willemsen <stenw@freeler.nl>
2017-11-17 19:11:54 +0100

    Adds UK lung data

--- data-raw/bmt.R
+++ data-raw/bmt.R
@@ -1,6 +1,5 @@
 library(devtools)
-doll <- foreign::read.spss(file='./data-raw/doll.sav', to.data.frame=TRUE)
-usethis::use_data(doll, overwrite = TRUE)
+
 bmt <- foreign::read.spss(file='./data-raw/bmt.sav', to.data.frame=TRUE)
 usethis::use_data(bmt, overwrite = TRUE)
 ova <- foreign::read.spss(file='./data-raw/ova.sav', to.data.frame=TRUE)
@@ -9,11 +8,20 @@ fracture <- foreign::read.spss(file='./data-raw/fracture.sav', to.data.frame=TRU
 usethis::use_data(fracture, overwrite = TRUE)
 LBW <- foreign::read.spss(file='./data-raw/LBW.sav', to.data.frame=TRUE)
 usethis::use_data(LBW, overwrite = TRUE)
-endom <- foreign::read.spss(file='./data-raw/endom.sav', to.data.frame=TRUE)
-usethis::use_data(endom, overwrite = TRUE)
 teeth <- read.table('./data-raw/teeth.dat', header = TRUE)
 usethis::use_data(teeth, overwrite = TRUE)
 epilepsy <- read.table('./data-raw/epilepsy.dat', header = TRUE)
 usethis::use_data(epilepsy, overwrite = TRUE)
 melanoma <- read.table('./data-raw/melanoma.dat', header = TRUE)
 usethis::use_data(melanoma, overwrite = TRUE)
+endopaired <- read.csv('./data-raw/endompared.csv', header = TRUE)
+usethis::use_data(endopaired, overwrite = TRUE)
+lung<-c(3035, 2552, 2704, 2554, 2014, 1655, 1721, 1524, 1596, 2074, 2199, 2512,
+        2933, 2889, 2938, 2497, 1870, 1726, 1607, 1545, 1396, 1787, 2076, 2837,
+        2787, 3891, 3179, 2011, 1636, 1580, 1489, 1300, 1356, 1653, 2013, 2823,
+        2996, 2523, 2540, 2520, 1994, 1641, 1691, 1479, 1596, 1877, 2032, 2484,
+        2899, 2990, 2890, 2379, 1933, 1734, 1617, 1495, 1440, 1777, 1970, 2745,
+        2841, 3535, 3010, 2091, 1667, 1589, 1518, 1349, 1392, 1619, 1954, 2633)
+matrix(lung, 12)
+lung_data <- data.frame(Y=lung, month=rep(1:12,6), year=rep(1974:1979, each=12))
+usethis::use_data(lung_data, overwrite = TRUE)
